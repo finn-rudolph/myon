9d9d557a5bf743c330f614621d09fbfbafb986fa
Finn Rudolph <finn.rudolph@t-online.de>
2023-06-24 09:22:05 +0200

    Explicitly store the number of rows of a CscMatrix
    Helps to catch bugs and uses less memory on matrices with n much
    greater than m.

--- src/linalg.rs
+++ src/linalg.rs
@@ -6,7 +6,8 @@ use std::ops::Mul;
 pub const N: usize = 64;
 
 pub struct CscMatrix {
-    end: Vec<u32>,
+    m: usize,      // number of rows
+    end: Vec<u32>, // number of columns = end.len()
     ones: Vec<u32>,
 }
 
@@ -21,8 +22,8 @@ pub struct TransposedBlockMatrix<'a> {
 }
 
 impl CscMatrix {
-    pub fn new(end: Vec<u32>, ones: Vec<u32>) -> CscMatrix {
-        CscMatrix { end, ones }
+    pub fn new(m: usize, end: Vec<u32>, ones: Vec<u32>) -> CscMatrix {
+        CscMatrix { m, end, ones }
     }
     pub fn len(&self) -> usize {
         self.end.len()
@@ -91,13 +92,14 @@ impl Mul<&BlockMatrix> for &CscMatrix {
     type Output = BlockMatrix;
 
     fn mul(self, b: &BlockMatrix) -> BlockMatrix {
-        let n = self.end.len();
-        let mut res = BlockMatrix::from(vec![0; n]);
+        let (n, m) = (self.end.len(), self.m);
+        assert_eq!(n, b.len());
+        let mut res = blockmatrix![0; m];
 
-        let mut j = 0;
+        let mut j: usize = 0;
         for i in 0..n {
-            while j < self.end[i] {
-                res[i] ^= b[self.ones[j as usize] as usize];
+            while j < self.end[i] as usize {
+                res[self.ones[j] as usize] ^= b[i];
                 j += 1;
             }
         }
@@ -110,13 +112,14 @@ impl<'a> Mul<&BlockMatrix> for &CsrMatrix<'a> {
     type Output = BlockMatrix;
 
     fn mul(self, b: &BlockMatrix) -> BlockMatrix {
-        let n = self.borrowed.end.len();
-        let mut res = BlockMatrix::from(vec![0; n]);
+        let (n, m) = (self.borrowed.end.len(), self.borrowed.m);
+        assert_eq!(m, b.len());
+        let mut res = blockmatrix![0; n];
 
-        let mut j = 0;
+        let mut j: usize = 0;
         for i in 0..n {
-            while j < self.borrowed.end[i] {
-                res[self.borrowed.ones[j as usize] as usize] ^= b[i];
+            while j < self.borrowed.end[i] as usize {
+                res[i] ^= b[self.borrowed.ones[j] as usize];
                 j += 1;
             }
         }
@@ -129,8 +132,9 @@ impl Mul<&BlockMatrix> for &BlockMatrix {
     type Output = BlockMatrix;
 
     fn mul(self, b: &BlockMatrix) -> BlockMatrix {
+        assert_eq!(N, b.len());
         let n = self.len();
-        let mut res = BlockMatrix::from(vec![0; n]);
+        let mut res = blockmatrix![0; n];
 
         for i in 0..n {
             let mut x = self[i];
@@ -152,9 +156,12 @@ impl<'a> Mul<&TransposedBlockMatrix<'a>> for &BlockMatrix {
     type Output = BlockMatrix;
 
     fn mul(self, b: &TransposedBlockMatrix<'a>) -> BlockMatrix {
-        let mut res = BlockMatrix::from(vec![0; N]);
+        let n = self.len();
+        assert!(n >= N);
+        assert_eq!(N, b.borrowed.len());
+        let mut res = blockmatrix![0; n];
 
-        for i in 0..N {
+        for i in 0..n {
             for j in 0..N {
                 res[i] |= (((self[i] & b.borrowed[j]).count_ones() & 1) as u64) << j;
             }
@@ -169,7 +176,8 @@ impl<'a> Mul<&BlockMatrix> for &TransposedBlockMatrix<'a> {
 
     fn mul(self, b: &BlockMatrix) -> BlockMatrix {
         let n = self.borrowed.len();
-        let mut res = BlockMatrix::from(vec![0; N]);
+        assert_eq!(b.len(), n);
+        let mut res = blockmatrix![0; N];
 
         for i in 0..n {
             let mut x = self.borrowed[i];
