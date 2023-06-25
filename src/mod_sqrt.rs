64b899777fe39ec95d6791624ec8684c2420eac3
Finn Rudolph <finn.rudolph@t-online.de>
2023-06-26 00:27:29 +0200

    Remove LKK reduction since it was wrong
    To calculate the remainder of a 64-bit integer (the result of a 32-bit
    multiplication), one would need 256-bit integers (or some weird tricks
    that aren't worth implementing at this stage).

--- src/mod_sqrt.rs
+++ src/mod_sqrt.rs
@@ -1,95 +1,43 @@
 use rand_xoshiro::{rand_core::RngCore, Xoshiro256PlusPlus};
 
 // TODO: make this module generic
+//       If this module becomes a bottleneck, use something like LKK / Montgomery
 
-pub trait ModReduction {
-    fn new(n: u32) -> Self;
-    fn rem(&self, a: u64) -> u32;
-}
-
-struct LemireReduction {
-    n: u32, // divisor / modulus
-    c: u64, // 2^64 / n + 1
-}
-
-// Can quickly compute a / n, a % m and test whether a is divisible by n, for arbitrary a and fixed
-// (but not compile-time known) n. Described in Lemire, D, Kaser, O. & Kurz, N. (2019).
-impl ModReduction for LemireReduction {
-    fn new(n: u32) -> LemireReduction {
-        assert!(n != 0);
-        LemireReduction {
-            n,
-            c: u64::MAX / (n as u64) + 1,
-        }
-    }
-
-    fn rem(&self, a: u64) -> u32 {
-        let lowbits = self.c.wrapping_mul(a as u64);
-        ((lowbits as u128 * self.n as u128) >> 64) as u32
-    }
-}
-
-impl LemireReduction {
-    fn div(&self, a: u32) -> u32 {
-        ((self.c as u128 * a as u128) >> 64) as u32
-    }
-
-    fn is_divisible(&self, a: u32) -> bool {
-        self.c.wrapping_mul(a as u64) < self.c
-    }
-}
-
-struct NativeReduction {
-    n: u32,
-}
-
-impl ModReduction for NativeReduction {
-    fn new(n: u32) -> NativeReduction {
-        NativeReduction { n }
-    }
-
-    fn rem(&self, a: u64) -> u32 {
-        (a % self.n as u64) as u32
-    }
-}
-
-fn mod_exp<T: ModReduction>(mut a: u64, mut b: u64, reduction: &T) -> u64 {
+const fn mod_exp(mut a: u64, mut b: u64, n: u64) -> u64 {
     let mut c: u64 = 1;
 
     while b != 0 {
         if b & 1 == 1 {
-            c = reduction.rem(c * a) as u64;
+            c = (c * a) % n;
         }
-        a = reduction.rem(a * a) as u64;
+        a = (a * a) % n;
         b >>= 1;
     }
 
     c
 }
 
-fn legendre<T: ModReduction>(a: u64, p: u64, lemire: &T) -> u64 {
-    mod_exp(a, (p - 1) >> 1, lemire)
+const fn legendre(a: u64, p: u64) -> u64 {
+    mod_exp(a, (p - 1) >> 1, p)
 }
 
-fn mod_inverse<T: ModReduction>(a: u64, m: u64, lemire: &T) -> u64 {
-    mod_exp(a, m - 2, lemire)
+const fn mod_inverse(a: u64, n: u64) -> u64 {
+    mod_exp(a, n - 2, n)
 }
 
 // Finds a square root of a modulo p using the Tonelli-Shanks algorithm. a and p may not be greater
 // than u32::MAX, since multiplication is performed with them.
-pub fn mod_sqrt<T: ModReduction>(mut a: u64, p: u64, xo: &mut Xoshiro256PlusPlus) -> u64 {
+pub fn mod_sqrt(mut a: u64, p: u64, xo: &mut Xoshiro256PlusPlus) -> u64 {
     assert!(a <= u32::MAX as u64);
     assert!(p <= u32::MAX as u64);
 
-    let reduction = T::new(p as u32);
-
     if p & 3 == 3 {
-        return mod_exp(a, (p + 1) >> 2, &reduction);
+        return mod_exp(a, (p + 1) >> 2, p);
     }
 
     // About 2 iterations are expected.
     let mut b = xo.next_u64() % p;
-    while b == 0 || legendre(b, p, &reduction) == 1 {
+    while b == 0 || legendre(b, p) == 1 {
         b = xo.next_u64() % p;
     }
 
@@ -98,11 +46,11 @@ pub fn mod_sqrt<T: ModReduction>(mut a: u64, p: u64, xo: &mut Xoshiro256PlusPlus
     let mut m = (p - 1) >> 2;
     let mut correction: u64 = 1;
     let mut c = b;
-    let mut cinv = mod_inverse(b, p, &reduction);
+    let mut cinv = mod_inverse(b, p);
 
     loop {
-        if mod_exp(a, m, &reduction) != 1 {
-            a = reduction.rem(a * reduction.rem(c * c) as u64) as u64;
+        if mod_exp(a, m, p) != 1 {
+            a = (a * ((c * c) % p)) % p;
             correction = (correction * cinv) % p;
         }
         if m & 1 == 1 {
@@ -113,7 +61,7 @@ pub fn mod_sqrt<T: ModReduction>(mut a: u64, p: u64, xo: &mut Xoshiro256PlusPlus
         cinv = (cinv * cinv) % p;
     }
 
-    reduction.rem(mod_exp(a, (m + 1) >> 1, &reduction) * correction) as u64
+    (mod_exp(a, (m + 1) >> 1, p) * correction) % p
 }
 
 // TODO: Add Cipolla's algorithm (it shall be faster sometimes?)
@@ -132,13 +80,12 @@ mod tests {
 
         let trailing_zeros = (n - 1).trailing_zeros();
         let u = (n - 1) >> trailing_zeros;
-        let lemire = LemireReduction::new(n as u32);
 
         for mut a in MILLER_RABIN_BASES {
             a = a % n;
-            let mut x = mod_exp(a, u, &lemire);
+            let mut x = mod_exp(a, u, n);
             for _ in 0..trailing_zeros {
-                let y = lemire.rem(x * x) as u64;
+                let y = (x * x) % n;
                 if y == 1 && x != 1 && x != n - 1 {
                     return false;
                 }
@@ -162,33 +109,16 @@ mod tests {
     }
 
     #[test]
-    fn test_lemire_reduction() {
-        let mut xo = Xoshiro256PlusPlus::seed_from_u64(1000000009);
-
-        for _ in 0..1000 {
-            let n = xo.next_u32();
-            let lemire = LemireReduction::new(n);
-            for _ in 0..1000 {
-                let a = xo.next_u32();
-                assert_eq!(lemire.div(a), a / n);
-                assert_eq!(lemire.rem(a as u64), a % n);
-                assert_eq!(lemire.is_divisible(a), a % n == 0);
-            }
-        }
-    }
-
-    #[test]
     fn test_tonelli_shanks() {
         let mut xo = Xoshiro256PlusPlus::seed_from_u64(1000000009);
 
-        for _ in 0..1000 {
+        for _ in 0..10000 {
             let p = gen_prime(&mut xo);
-            let lemire = LemireReduction::new(p);
             let mut a = xo.next_u32() % p;
-            while legendre(a as u64, p as u64, &lemire) != 1 {
+            while legendre(a as u64, p as u64) != 1 {
                 a = xo.next_u32() % p;
             }
-            let x = mod_sqrt::<NativeReduction>(a as u64, p as u64, &mut xo);
+            let x = mod_sqrt(a as u64, p as u64, &mut xo);
             assert_eq!((x * x) % p as u64, a as u64);
         }
     }
