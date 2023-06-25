bb0859664a40228302155d2bbda5aec17590ef2c
Finn Rudolph <finn.rudolph@t-online.de>
2023-06-25 09:08:06 +0200

    Change default type in Tonelli-Shanks to u64
    This avoids frequent casts, which are necessary due to multiplication.

--- src/mod_arithmetic.rs
+++ src/mod_arithmetic.rs
@@ -1,43 +1,47 @@
 use rand_xoshiro::{rand_core::RngCore, Xoshiro256PlusPlus};
 
-fn mod_exp(mut a: u32, mut b: u32, m: u32) -> u32 {
+fn mod_exp(mut a: u64, mut b: u64, m: u64) -> u64 {
     let mut c: u64 = 1;
 
     while b != 0 {
         if b & 1 == 1 {
-            c = (c * a as u64) % m as u64;
+            c = (c * a) % m;
         }
         a = (a * a) % m;
         b >>= 1;
     }
 
-    c as u32
+    c
 }
 
-fn legendre(a: u32, p: u32) -> u32 {
+fn legendre(a: u64, p: u64) -> u64 {
     mod_exp(a, (p - 1) >> 1, p)
 }
 
-fn mod_inverse(a: u32, m: u32) -> u32 {
+fn mod_inverse(a: u64, m: u64) -> u64 {
     mod_exp(a, m - 2, m)
 }
 
-// Finds a square root of a modulo p using the Tonelli-Shanks algorithm.
-pub fn mod_sqrt(mut a: u32, p: u32, xo: &mut Xoshiro256PlusPlus) -> u32 {
+// Finds a square root of a modulo p using the Tonelli-Shanks algorithm. a and p may not be greater
+// than u32::MAX, since multiplication is performed with them.
+pub fn mod_sqrt(mut a: u64, p: u64, xo: &mut Xoshiro256PlusPlus) -> u64 {
+    assert!(a <= u32::MAX as u64);
+    assert!(p <= u32::MAX as u64);
+
     if p & 3 == 3 {
         return mod_exp(a, (p + 1) >> 2, p);
     }
 
     // About 2 iterations are expected.
-    let mut b = xo.next_u32() % p;
+    let mut b = xo.next_u64() % p;
     while b == 0 || legendre(b, p) == 1 {
-        b = xo.next_u32() % p;
+        b = xo.next_u64() % p;
     }
 
     // Loop invariant: c = b ^ (2 ^ (k - 2)). Before the loop, k = 2, which is possible since p = 1
     // mod 4, so m = (p - 1) / 2^k is an integer.
     let mut m = (p - 1) >> 2;
-    let mut correction = 1;
+    let mut correction: u64 = 1;
     let mut c = b;
     let mut cinv = mod_inverse(b, p);
 
