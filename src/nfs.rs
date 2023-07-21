1d50539d9c22a33d1c94af9579a6a667646185b9
Finn Rudolph <finn.rudolph@t-online.de>
2023-07-21 11:32:19 +0200

    Store parameters as a struct
    Better than having some obscure global variables.

--- src/nfs.rs
+++ src/nfs.rs
@@ -2,33 +2,23 @@ use rug::{ops::Pow, Integer};
 
 use crate::nt;
 
-mod params {
-    use rug::Integer;
-
-    static mut RATIONAL_BASE_SIZE: usize = 0;
-    static mut ALGEBRAIC_BASE_SIZE: usize = 0;
-    static mut POLYNOMIAL_DEGREE: u32 = 0;
-
-    pub fn rational_base_size() -> usize {
-        unsafe { RATIONAL_BASE_SIZE }
-    }
-
-    pub fn algebraic_base_size() -> usize {
-        unsafe { ALGEBRAIC_BASE_SIZE }
-    }
-
-    pub fn polynomial_degree() -> u32 {
-        unsafe { POLYNOMIAL_DEGREE }
-    }
-
-    pub fn set_params(n: &Integer) {}
+struct Params {
+    rational_base_size: usize,
+    algebraic_base_size: usize,
+    polynomial_degree: u32,
 }
 
-fn rational_factor_base(size: usize) -> Vec<u32> {
+impl Params {
+    fn new(n: &Integer) -> Params {
+        todo!()
+    }
+}
+
+fn rational_factor_base(params: &Params) -> Vec<u32> {
     let mut base: Vec<u32> = Vec::new();
 
     let mut i: u32 = 2;
-    while base.len() < size {
+    while base.len() < params.rational_base_size {
         if nt::is_prime(i) {
             base.push(i);
         }
@@ -39,18 +29,18 @@ fn rational_factor_base(size: usize) -> Vec<u32> {
 }
 
 // Returns two integers t and m, the selected polynomial is x^d - t. f(m) = 0 mod n.
-fn select_polynomial(r: u32, e: u32, s: i32) -> (i32, Integer) {
-    let d = params::polynomial_degree();
+fn select_polynomial(r: u32, e: u32, s: i32, params: &Params) -> (i32, Integer) {
+    let d = params.polynomial_degree;
     let k = (e + d - 1) / d;
     (s * r.pow(k * d - e) as i32, Integer::from(r).pow(k))
 }
 
-fn find_polynomial_roots(t: i32, p: u32) -> Vec<u32> {
-    let d = params::rational_base_size();
+fn find_polynomial_roots(t: i32, p: u32, params: &Params) -> Vec<u32> {
+    let d = params.rational_base_size;
     let mut roots: Vec<u32> = Vec::new();
 
     for i in 1..p {
-        if i.pow(d as u32) as i32 - t == 0 {
+        if (i.pow(d as u32) as i32 - t) % p as i32 == 0 {
             roots.push(i);
         }
     }
@@ -58,16 +48,16 @@ fn find_polynomial_roots(t: i32, p: u32) -> Vec<u32> {
     roots
 }
 
-fn algebraic_factor_base(size: usize, t: i32) -> Vec<(u32, u32)> {
+fn algebraic_factor_base(t: i32, params: &Params) -> Vec<(u32, u32)> {
     let mut base: Vec<(u32, u32)> = Vec::new();
-    let mut p: u32 = 3;
+    let mut p: u32 = 2;
 
-    while base.len() < size {
+    while base.len() < params.algebraic_base_size {
         if nt::is_prime(p) {
-            let roots = find_polynomial_roots(t, p);
+            let roots = find_polynomial_roots(t, p, params);
             base.extend(roots.iter().map(|r| (p, *r)));
         }
-        p += 2;
+        p += 1;
     }
 
     base
@@ -75,7 +65,12 @@ fn algebraic_factor_base(size: usize, t: i32) -> Vec<(u32, u32)> {
 
 pub fn factorize(r: u32, e: u32, s: i32) -> String {
     const x: u64 = 1 << 43;
-    let rational_base = rational_factor_base(500);
+    let n: Integer = Integer::from(r).pow(e) - s;
+
+    let params = Params::new(&n);
+    let (t, m) = select_polynomial(r, e, s, &params);
+    let rational_base = rational_factor_base(&params);
+    let algebraic_base = algebraic_factor_base(t, &params);
 
     todo!()
 }
