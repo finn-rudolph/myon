af71fcbdfb81fa9125e1d1d0cb7cede4993f912a
Finn Rudolph <finn.rudolph@t-online.de>
2023-07-21 11:58:04 +0200

    Update main.rs and move it to src

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::io;
+
+fn read_int<T: std::str::FromStr>() -> T
+where
+    <T as std::str::FromStr>::Err: std::fmt::Debug,
+{
+    let mut buf = String::new();
+    io::stdin()
+        .read_line(&mut buf)
+        .expect("Failed to read integer.");
+    buf.trim().parse().expect("Input is not a valid integer.")
+}
+
+fn main() {
+    env::set_var("RUST_LOG", "info");
+    env_logger::init();
+
+    println!("You can factor numbers of the form r^e - s, where r and |s| are preferably small.");
+    print!("r: ");
+    let r = read_int::<u32>();
+    print!("e: ");
+    let e = read_int::<u32>();
+    print!("s: ");
+    let s = read_int::<i32>();
+
+    let (a, b) = nfs::factorize(r, e, s);
+    println!("Number factored as {} * {}", a, b);
+}
