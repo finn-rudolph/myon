38e705cebe4f2c9bbd45464242eb95f28dd08e30
Finn Rudolph <finn.rudolph@t-online.de>
2023-07-21 16:31:01 +0200

    Implement trial division and change sieving
    Give every sieve location on the algebraic side it's own initial value.
    Maybe this technique can be optimized to be actually useful, but for
    now, it makes the sieving process somewhat more robust.

--- src/lib.rs
+++ src/lib.rs
@@ -6,92 +6,96 @@ use rug::{integer::IsPrime, Complete, Integer};
 
 mod lanczos;
 mod linalg;
+mod nfs;
 mod nt;
-mod qs;
 
 #[derive(Debug)]
-pub struct MyonError {
+pub struct NfsError {
     message: String,
 }
 
-impl MyonError {
-    fn new(message: &str) -> MyonError {
-        MyonError {
+impl NfsError {
+    fn new(message: &str) -> NfsError {
+        NfsError {
             message: message.to_string(),
         }
     }
 }
 
-impl Error for MyonError {}
+impl Error for NfsError {}
 
-impl fmt::Display for MyonError {
+impl fmt::Display for NfsError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.message)
     }
 }
 
-pub fn factorize(n_str: &str) -> Result<Vec<(String, u32)>, MyonError> {
-    // Keep a queue of factors of n that still need to be factored (or recognized as primes).
-    let n: Integer = match Integer::parse(n_str) {
-        Ok(v) => v.complete(),
-        Err(_) => {
-            return Err(MyonError::new(
-                "The given string could not be parsed as an integer.",
-            ))
-        }
-    };
-
-    if n < 0 {
-        return Err(MyonError::new(
-            "The given integer is negative. Only positive integers are allowed.",
-        ));
-    }
-
-    let mut ints_to_factor: Vec<Integer> = vec![n];
-    let mut primes: Vec<Integer> = vec![];
-
-    const MILLER_RABIN_ITERATIONS: u32 = 74;
-
-    while !ints_to_factor.is_empty() {
-        let mut n = ints_to_factor.pop().unwrap();
-        let mut exponent_multiplier: u32 = 1;
-
-        // The quadratic sieve requires that n must not be a power, so extract roots until this is
-        // satisfied. (TODO: Maybe optimize this to do it only on primes.)
-        let mut i: u32 = 2;
-        while n.is_perfect_power() {
-            let (root, remainder) = n.root_rem_ref(i).complete();
-            if remainder == 0 {
-                exponent_multiplier *= i;
-                n = root;
-            }
-            i += 1;
-        }
-
-        if n.is_probably_prime(MILLER_RABIN_ITERATIONS) != IsPrime::No {
-            primes.extend(iter::repeat(n).take(exponent_multiplier as usize));
-        } else {
-            let (r, s) = qs::factorize(&n);
-            for x in [r, s] {
-                ints_to_factor.extend(iter::repeat(x).take(exponent_multiplier as usize));
-            }
-        }
-    }
-
-    // Group equal prime factors together and write each factor and it's exponent in a tuple.
-    primes.sort();
-    let mut factorization: Vec<(String, u32)> = vec![];
-
-    let mut i = 0;
-    while i < primes.len() {
-        factorization.push((primes[i].to_string(), 1));
-        let mut j = i + 1;
-        while j < primes.len() && primes[j] == primes[i] {
-            factorization.last_mut().unwrap().1 += 1;
-            j += 1;
-        }
-        i = j;
-    }
-
-    Ok(factorization)
+pub fn factorize(r: u32, e: u32, s: i32) -> (String, String) {
+    todo!()
 }
+
+// pub fn factorize(n_str: &str) -> Result<Vec<(String, u32)>, NfsError> {
+//     // Keep a queue of factors of n that still need to be factored (or recognized as primes).
+//     let n: Integer = match Integer::parse(n_str) {
+//         Ok(v) => v.complete(),
+//         Err(_) => {
+//             return Err(NfsError::new(
+//                 "The given string could not be parsed as an integer.",
+//             ))
+//         }
+//     };
+
+//     if n < 0 {
+//         return Err(NfsError::new(
+//             "The given integer is negative. Only positive integers are allowed.",
+//         ));
+//     }
+
+//     let mut ints_to_factor: Vec<Integer> = vec![n];
+//     let mut primes: Vec<Integer> = vec![];
+
+//     const MILLER_RABIN_ITERATIONS: u32 = 74;
+
+//     while !ints_to_factor.is_empty() {
+//         let mut n = ints_to_factor.pop().unwrap();
+//         let mut exponent_multiplier: u32 = 1;
+
+//         // The quadratic sieve requires that n must not be a power, so extract roots until this is
+//         // satisfied. (TODO: Maybe optimize this to do it only on primes.)
+//         let mut i: u32 = 2;
+//         while n.is_perfect_power() {
+//             let (root, remainder) = n.root_rem_ref(i).complete();
+//             if remainder == 0 {
+//                 exponent_multiplier *= i;
+//                 n = root;
+//             }
+//             i += 1;
+//         }
+
+//         if n.is_probably_prime(MILLER_RABIN_ITERATIONS) != IsPrime::No {
+//             primes.extend(iter::repeat(n).take(exponent_multiplier as usize));
+//         } else {
+//             let (r, s) = nfs::factorize(&n);
+//             for x in [r, s] {
+//                 ints_to_factor.extend(iter::repeat(x).take(exponent_multiplier as usize));
+//             }
+//         }
+//     }
+
+//     // Group equal prime factors together and write each factor and it's exponent in a tuple.
+//     primes.sort();
+//     let mut factorization: Vec<(String, u32)> = vec![];
+
+//     let mut i = 0;
+//     while i < primes.len() {
+//         factorization.push((primes[i].to_string(), 1));
+//         let mut j = i + 1;
+//         while j < primes.len() && primes[j] == primes[i] {
+//             factorization.last_mut().unwrap().1 += 1;
+//             j += 1;
+//         }
+//         i = j;
+//     }
+
+//     Ok(factorization)
+// }
--- src/nfs.rs
+++ src/nfs.rs
@@ -1,4 +1,6 @@
-use rug::{ops::Pow, Integer};
+use std::cmp::max;
+
+use rug::{ops::Pow, Complete, Integer};
 
 use crate::nt;
 
@@ -7,6 +9,8 @@ struct Params {
     rational_base_size: usize,
     algebraic_base_size: usize,
     polynomial_degree: u32,
+    sieve_array_size: usize,
+    fudge: u8, // different fudge for rational and algebraic side?
 }
 
 impl Params {
@@ -16,6 +20,8 @@ impl Params {
             rational_base_size: 500,
             algebraic_base_size: 500,
             polynomial_degree: 3,
+            sieve_array_size: 10000,
+            fudge: 20,
         },
     )];
 
@@ -32,15 +38,15 @@ impl Params {
     }
 }
 
-fn rational_factor_base(params: &Params) -> Vec<u32> {
-    let mut base: Vec<u32> = Vec::new();
+fn rational_factor_base(m: &Integer, params: &Params) -> Vec<(u32, u32)> {
+    let mut base: Vec<(u32, u32)> = Vec::new();
 
-    let mut i: u32 = 2;
+    let mut p: u32 = 2;
     while base.len() < params.rational_base_size {
-        if nt::is_prime(i) {
-            base.push(i);
+        if nt::is_prime(p) {
+            base.push((p, m.mod_u(p)));
         }
-        i += 1;
+        p += 1;
     }
 
     base
@@ -81,13 +87,69 @@ fn algebraic_factor_base(t: i32, params: &Params) -> Vec<(u32, u32)> {
     base
 }
 
-pub fn factorize(r: u32, e: u32, s: i32) -> String {
+fn ilog2_rounded(x: u32) -> u32 {
+    ((x * x).ilog2() + 1) >> 1
+}
+
+fn line_sieve(b: u32, sieve_array: &mut Vec<i8>, base: &Vec<(u32, u32)>) {
+    let a0: i32 = -(sieve_array.len() as i32 / 2);
+
+    for (p, r) in base {
+        let log2p = ilog2_rounded(*p) as i8;
+        let mut i = ((-((b * r) as i32) % *p as i32 + *p as i32 - a0) % *p as i32) as usize;
+        while i < sieve_array.len() {
+            sieve_array[i] += log2p;
+            i += *p as usize;
+        }
+    }
+}
+
+pub fn factorize(r: u32, e: u32, s: i32) -> Integer {
     let n: Integer = Integer::from(r).pow(e) - s;
 
     let params = Params::new(&n);
+    let d = params.polynomial_degree;
     let (t, m) = select_polynomial(r, e, s, &params);
-    let rational_base = rational_factor_base(&params);
+    let rational_base = rational_factor_base(&m, &params);
     let algebraic_base = algebraic_factor_base(t, &params);
+
+    let mut rational_sieve_array: Vec<i8> = vec![0; params.sieve_array_size];
+    let mut algebraic_sieve_array: Vec<i8> = vec![0; params.sieve_array_size];
+
+    for b in 1.. {
+        rational_sieve_array.fill(-((ilog2_rounded(b) + m.significant_bits()) as i8));
+        line_sieve(b, &mut rational_sieve_array, &rational_base);
+
+        let log_tbd = (d * ilog2_rounded(b) + ilog2_rounded(t.abs() as u32)) as i8;
+        let a0 = -(params.sieve_array_size as i32 / 2);
+        for i in 0..params.sieve_array_size {
+            algebraic_sieve_array[i] = max((d as i32 * (a0 + i as i32)) as i8, log_tbd);
+        }
+        line_sieve(b, &mut algebraic_sieve_array, &algebraic_base);
+
+        // Consider unsafe access here to avoid bounds checks.
+        for i in 0..params.sieve_array_size {
+            if rational_sieve_array[i] >= 0 && algebraic_sieve_array[i] >= 0 {
+                let a = a0 + i as i32;
+
+                // Trial divide on the rational side.
+                let mut x = a + (b * &m).complete();
+                for (p, _) in &rational_base {
+                    let e = x.remove_factor_mut(&Integer::from(*p));
+                }
+
+                // Trial divide on the algrbraic side.
+                let mut y = Integer::from(a).pow(d) - t * Integer::from(-(b as i32)).pow(d);
+                for (p, _) in &algebraic_base {
+                    let e = y.remove_factor_mut(&Integer::from(*p));
+                }
+
+                if x == 1 && y == 1 {
+                    // smooth pair (a, b) found!
+                }
+            }
+        }
+    }
 
     todo!()
 }
